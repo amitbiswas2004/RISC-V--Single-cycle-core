fd9dd37be2a337edf31ca4c5e60e94cc6b0fa80f
amitbiswas2004 <amitkbiswas1234@gmail.com>
2026-04-25 19:22:44 +0530

    Update sign_extend.v

--- rtl/core/sign_extend.v
+++ rtl/core/sign_extend.v
@@ -1,22 +1,9 @@
-`timescale 1ns/1ps
-
-module sign_extend #(
-    parameter XLEN = 32
-) (
-    input  wire [31:0] instruction,
-    output reg  [XLEN-1:0] imm_ext
+module sign_extend(
+    input [31:0] in,
+    output [31:0] imm_extension
 );
-    wire [6:0] opcode;
-    assign opcode = instruction[6:0];
 
-    localparam OPCODE_OP_IMM = 7'b0010011; // addi
-    localparam OPCODE_LOAD   = 7'b0000011; // lw
+assign imm_extension = (in[31]) ? {{20{1'b1}}, in[31:20]} 
+                                : {{20{1'b0}}, in[31:20]};
 
-    always @(*) begin
-        case (opcode)
-            OPCODE_OP_IMM,
-            OPCODE_LOAD: imm_ext = {{20{instruction[31]}}, instruction[31:20]};
-            default:     imm_ext = {XLEN{1'b0}};
-        endcase
-    end
 endmodule
